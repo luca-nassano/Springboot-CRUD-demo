1dea9f6119bd38616d709cf850f3136fa36b8753
Luca Nassano <luca.nassano@mail.polimi.it>
2020-07-02 14:57:53 +0200

    added db

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -2,9 +2,11 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -22,7 +24,17 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        final String sql = "INSERT INTO person (id, name) VALUES (?,?)";
+        try {
+            jdbcTemplate.update(
+                    sql,
+                    id,
+                    person.getName()
+            );
+            return 1;
+        } catch (DataAccessException dae) {
+            return 0;
+        }
     }
 
     @Override
@@ -55,11 +67,28 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        final String sql = "DELETE FROM person WHERE id = ?";
+        try {
+            jdbcTemplate.update(sql, id);
+            return 1;
+        } catch (DataAccessException dae){
+            return 0;
+        }
     }
 
     @Override
     public int updatePersonById(UUID id, Person person) {
-        return 0;
+        final String sql = "UPDATE person SET name = ? WHERE id = ?";
+        try {
+            jdbcTemplate.update(
+                    sql,
+                    person.getName(),
+                    id);
+            return 1;
+        } catch (DataAccessException dae){
+            return 0;
+        }
     }
+
+
 }
